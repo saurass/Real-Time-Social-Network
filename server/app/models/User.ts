4b1cf5865fbde58d2c05dbd04d7cdad627169950
saurass <saurabhsrivastava312@gmail.com>
2018-07-08 16:19:33 +0530

    Added social login schema to users model

--- server/app/models/User.ts
+++ server/app/models/User.ts
@@ -5,7 +5,25 @@ let userSchema = mongoose.Schema({
     username: {type: String, unique: true},
     email: {type: String, unique: true},
     password: {type: String, unique: true}
-  }
+  },
+  facebook: {
+    id: {type: String, unique: true},
+    token: {type: String},
+    name: {type: String},
+    email: {type: String}
+  },
+  twitter: {
+    id: {type: String, unique: true},
+    token: {type: String},
+    displayName: {type: String},
+    username: {type: String}
+  },
+  google: {
+    id: {type: String, unique: true},
+    token: {type: String},
+    name: {type: String},
+    email: {type: String}
+  },
 });
 
 userSchema = mongoose.model('User', userSchema);
